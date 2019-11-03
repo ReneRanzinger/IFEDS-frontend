5e886c5ddc31dd3173c72121eccca5e3dbcd51e3
Anubhav <nigam2294anu@gmail.com>
2019-11-03 12:38:50 -0500

    Cleaning Code. Defined structure in previous commit. Resolving Conflit issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 
 import App from './App';
 import { BrowserRouter, Route } from "react-router-dom";
@@ -18,13 +16,6 @@ const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
-=======
-//import './index.css';
-import './index.sass';
-import App from './App';
-import './index.css';
-
->>>>>>> 76d77c97a2349a9b6c6e04d5d5bbe4070c233bb1
 
 if (localStorage.appJWT) {
   const payload = decode(localStorage.appJWT);
