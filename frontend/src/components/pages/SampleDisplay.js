0ddcb5703afe493fd9368bc69c2d51fc15cf473b
Anubhav <nigam2294anu@gmail.com>
2019-12-20 12:36:40 -0500

    SampleList and Display List not working. - problem with previous commit of Sidebar - props were not passed to children - in future use react useSelector hook for connecting to redux store

--- frontend/src/components/pages/SampleDisplay.js
+++ frontend/src/components/pages/SampleDisplay.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import SideBar from "./Sidebar";
+import {connect} from "react-redux";
 import SampleList from "./SampleList";
+import {logout} from "../../actions/auth";
+import PropTypes  from 'prop-types';
+
 
 class SampleDisplay extends Component {
   render() {
     return (
       <div>
-        <SideBar />
-        <SampleList />
+        <SideBar props={this.props} isDashBoard={"true"}/>
+        <SampleList prop={this.props}/>
       </div>
     );
   }
 }
 
-export default SampleDisplay;
+SampleDisplay.propTypes = {
+  logout: PropTypes.func
+}
+
+function mapStateToProps(state) {
+  return {isAuthenticated: state.user.token};
+}
+export default connect(mapStateToProps, {logout})(SampleDisplay);
