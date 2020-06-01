dc85da329c9fa13c4a1436c14bbeb4fc1ace4db8
Anubhav <nigam2294anu@gmail.com>
2020-06-01 01:28:16 -0400

    Edit Sample Validation

---
+++ frontend/src/utils/validationConstant.js
@@ -0,0 +1,5 @@
+export const nullField = "This field cannot be null. Please fill"
+export const lengthField = "Length should be less than 50 characters"
+export const lengthField1 = "Length should be less than 64 characters"
+export const lengthField2 = "Length should be less than 256 characters"
+export const lengthField3 = "Length should be less than 1000 characters"
