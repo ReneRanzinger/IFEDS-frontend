6df89798891b5808ae66a41ce97fe04a23d696d0
Anubhav <nigam2294anu@gmail.com>
2020-07-31 11:27:43 -0400

    Login Validation

--- frontend/src/utils/validationConstant.js
+++ frontend/src/utils/validationConstant.js
@@ -3,3 +3,14 @@ export const lengthField = "Length should be less than 50 characters"
 export const lengthField1 = "Length should be less than 64 characters"
 export const lengthField2 = "Length should be less than 256 characters"
 export const lengthField3 = "Length should be less than 1000 characters"
+export const username = "Minimum Eight Characters"
+export const password = "Minimum eight characters, at least one letter and one number"
+export const wrongCredentials = "Incorrect Username or Password"
+
+export default   (res) => {
+  if(res.ok) {
+    return [res.text(), false]
+  } else {
+    return [res.json(),true]
+  }
+}
